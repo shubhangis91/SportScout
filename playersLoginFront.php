08a4b9baeec2a948a61a08fe92e4729372f98f34
VishalUzumaki <vishal41997@gmail.com>
2019-11-21 20:52:04 -0500

    players login form and check

---
+++ playersLoginFront.php
@@ -0,0 +1,22 @@
+
+<?php
+//Players login page
+
+echo 
+"
+<center>
+<form method='post' action='playersLoginBack.php'>
+UserName:<input type='text' name='playersUsername' placeholfer='vishalp04' />
+<br>
+<br>
+Password:<input name='playersPassword' type='text' />
+<br>
+<br>
+<button type='submit' name='playerLogin'>Login In</button>
+</form>
+</center>
+";
+
+
+
+?>
